289bbcd4b1e6ba0b421eb557be14906f41ee917e
Pearse Darcy <pearse.darcy@outlook.com>
2024-05-24 14:01:20 +0200

    feat: Add dark mode. CODE PEN FROM: https://codepen.io/j_holtslander/pen/MRbpLX

--- flask-app-walkthrough/skatehills/static/js/script.js
+++ flask-app-walkthrough/skatehills/static/js/script.js
@@ -3,4 +3,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+});
+  
+// SIDENAV
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+
+
+// SWAP ICON ON CLICK
+// Source: https://stackoverflow.com/a/34254979/751570
+$('.dark-toggle').on('click',function(){
+  if ($(this).find('i').text() == 'brightness_4'){
+      $(this).find('i').text('brightness_high');
+  } else {
+      $(this).find('i').text('brightness_4');
+  }
+});
+
+
+});
