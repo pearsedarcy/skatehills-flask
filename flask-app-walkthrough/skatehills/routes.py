fd63815fdbe2381974e8b9497b876a25e826ed31
Pearse Darcy <pearse.darcy@outlook.com>
2024-05-24 13:49:03 +0200

    feat: Add events page

--- flask-app-walkthrough/skatehills/routes.py
+++ flask-app-walkthrough/skatehills/routes.py
@@ -8,3 +8,9 @@ from skatehills.models import Event, User
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/events")
+def events():
+    events = Event.query.all()
+    return render_template("events.html", events=events)
---
+++ flask-app-walkthrough/skatehills/static/js/script.js
@@ -0,0 +1,6 @@
+
+// sidebar nav materialize
+document.addEventListener('DOMContentLoaded', function () {
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
